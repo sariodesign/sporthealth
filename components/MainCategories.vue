7ccc9a881dd0cdce78e9c8a6d4f679c4ce430194
Rosario <rosario.russo@blastingnews.com>
2020-02-06 14:49:31 +0100

    Created MainCategories component

---
+++ components/MainCategories.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="c-categories">
+    <h1>SPORT HEALTH: FISIOTERAPIA E RIATLETIZZAZIONE A ROMA E FUORI CITTÀ</h1>
+    <div v-for="category in categories" class="c-categories__item">
+      <h2>{{category.name}}</h2>
+      <p>{{category.text}}</p>
+    </div> 
+  </section>
+</template>
+<script>
+export default{
+  data: {
+    categories: [
+      { name: 'Fisioterapia',
+        background: '',
+        text: 'Le terapie manuali rappresentano il primo approccio riabilitativo',
+        link: ''},
+      { name: 'Idrokinesiterapia',
+        background: '',
+        text: 'L’utilizzo dell’acqua come migliore alleata a scopo terapeutico',
+        link: ''},
+      { name: 'Riatletizzazione',
+        background: '',
+        text: 'Un trainer personale per migliorare l’efficienza fisica e le performance di atleti',
+        link: ''},
+      { name: 'Neuromotoria',
+        background: '',
+        text: 'L’apprendimento o il riapprendimento del movimento',
+        link: ''},
+      { name: 'FisioEstetica',
+        background: '',
+        text: '',
+        link: ''}
+    ]
+  }
+}
+</script>
