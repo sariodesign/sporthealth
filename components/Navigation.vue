5c5677f4338dce70f2c316399f925e6c8a7081df
Rosario Russo <sariodesign.it@gmail.com>
2020-02-13 08:51:12 +0100

    Updated menu and navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,29 +1,35 @@
 <template>
   <nav class="c-navigation flex flex-col">
     <ul class="c-navigation__contact flex justify-end">
-      <li>
+      <li class="c-navigation__item">
         <nuxt-link class="c-navigation__link" to="/chi-siamo"
           >Chi siamo</nuxt-link
         >
       </li>
-      <li>
+      <li class="c-navigation__item">
         <nuxt-link class="c-navigation__link" to="/staff">Staff</nuxt-link>
       </li>
-      <li>
+      <li class="c-navigation__item">
         <nuxt-link class="c-navigation__link" to="/contatti"
           >Contatti</nuxt-link
         >
       </li>
-      <li>
+      <li class="c-navigation__item">
         <nuxt-link class="c-navigation__link" to="/sedi">Sedi</nuxt-link>
       </li>
     </ul>
-    <ul class="c-navigation__menu flex">
-      <li>
-        <nuxt-link class="c-navigation__link" to="/fisioterapia"
-          >Fisioterapia</nuxt-link
-        >
-      </li>
-    </ul>
+    <div class="c-navigation--primary">
+      <Menu />
+    </div>
   </nav>
 </template>
+<script>
+
+import Menu from '~/components/Menu.vue'
+
+export default {
+  components: {
+    Menu
+  }
+}
+</script>
