eb9d9942a2a12306c56c609813d189d819045e7a
Rosario <rosario.russo@blastingnews.com>
2020-02-27 18:50:35 +0100

    Added toggle method

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,35 +1,48 @@
 <template>
-  <nav class="c-navigation flex">
-    <ul class="c-navigation__contact flex justify-end">
-      <li class="c-navigation__item">
-        <nuxt-link class="c-navigation__link" to="/chi-siamo"
-          >Chi siamo</nuxt-link
-        >
-      </li>
-      <li class="c-navigation__item">
-        <nuxt-link class="c-navigation__link" to="/staff">Staff</nuxt-link>
-      </li>
-      <li class="c-navigation__item">
-        <nuxt-link class="c-navigation__link" to="/contatti"
-          >Contatti</nuxt-link
-        >
-      </li>
-      <li class="c-navigation__item">
-        <nuxt-link class="c-navigation__link" to="/sedi">Sedi</nuxt-link>
-      </li>
-    </ul>
-    <div class="c-navigation--primary">
-      <Menu />
+  <div>
+    <div class="c-navigation__toggle" :class="{'is-opacity' : opacityToggle}" @click="openMenu()">
+      <div class="c-navigation__toggle-line"></div>
     </div>
-  </nav>
+    <nav class="c-navigation flex" :class="{'is-open' : isOpen }">
+      <ul class="c-navigation__contact flex justify-end">
+        <li class="c-navigation__item">
+          <nuxt-link class="c-navigation__link" to="/chi-siamo">Chi siamo</nuxt-link>
+        </li>
+        <li class="c-navigation__item">
+          <nuxt-link class="c-navigation__link" to="/staff">Staff</nuxt-link>
+        </li>
+        <li class="c-navigation__item">
+          <nuxt-link class="c-navigation__link" to="/contatti">Contatti</nuxt-link>
+        </li>
+        <li class="c-navigation__item">
+          <nuxt-link class="c-navigation__link" to="/sedi">Sedi</nuxt-link>
+        </li>
+      </ul>
+      <div class="c-navigation--primary">
+        <Menu />
+      </div>
+    </nav>
+  </div>  
 </template>
+
 <script>
 
 import Menu from '~/components/Menu.vue'
 
 export default {
+  data () {
+    return {
+      isOpen: false,
+      opacityToggle: false
+    }
+  },
   components: {
     Menu
+  },
+  methods: {
+    openMenu: function() {
+      this.isOpen = !this.isOpen
+    }
   }
 }
 </script>
