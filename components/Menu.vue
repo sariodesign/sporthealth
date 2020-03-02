26fc891f5681712b8fa46c4e209c163d6e5a2095
Rosario <rosario.russo@blastingnews.com>
2020-03-02 18:16:16 +0100

    Completed function for mobile menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -10,8 +10,8 @@
                 {{item.page}}
             </nuxt-link>
             <div v-if="item.submenu">
-              <div class="c-navigation__toggle-submenu" :class="{'is-active' : isExpand}" v-on:click.stop="openSubmenu()"></div>
-              <ul class="c-navigation__submenu" :class="{'is-expanded' : isExpand}">
+              <div class="c-navigation__toggle-submenu" v-on:click="openSubmenu($event.target)"></div>
+              <ul class="c-navigation__submenu">
                 <li class="c-navigation__subitem" v-for="subitem in item.submenu" :key="subitem.id">
                   <nuxt-link class="c-navigation__link" :to=subitem.link>
                     {{subitem.page}}
@@ -23,69 +23,83 @@
     </ul>
 </template>
 <script>
-export default {
-  data () {
-    return {
-      items: [
-        { page: 'Fisioterapia',
-          link: '/fisioterapia',
-          submenu: [
-            {
-              page: 'Massoterapia',
-              link: '/massoterapia'
-            },
-            {
-              page: 'Rieducazione posturale',
-              link: '/rieducazione-posturale'
-            },
-            {
-              page: 'Pavimento pelvico',
-              link: '/pavimento-pelvico'
-            },
-            {
-              page: 'Tecarterapia',
-              link: '/tecarterapia'
-            },
-            {
-              page: 'Laserterapia',
-              link: '/laserterapia'
-            },
-            {
-              page: 'Onde d urto',
-              link: '/onde-urto'
-            },
-            {
-              page: 'Magnetoterapia',
-              link: '/maggnetoterapia'
-            },
-            {
-              page: 'Tariffario',
-              link: '/tariffario'
-            }
-          ]
-        },
-        { page: 'Osteopatia',
-          link: '/osteopatia'},
-        { page: 'Idrokinesiterapia',
-          link: '/idrokinesiterapia'},
-        { page: 'Riatletizzazione',
-          link: '/riatletizzazione'},
-        { page: 'Neuromotoria',
-          link: 'neuromotoria'},
-        { page: 'Consulenze',
-          link: '/consulenze',
-          submenu: true
-        },
-        { page: 'Prodotti',
-          link: 'prodotti'}
-      ],
-      isExpand: false 
-    }
-  },
-  methods: {
-    openSubmenu: function() {
-      this.isExpand = !this.isExpand
-    }
-  } 
-}
+  export default {
+    data () {
+      return {
+        items: [
+          { page: 'Fisioterapia',
+            link: '/fisioterapia',
+            submenu: [
+              {
+                page: 'Massoterapia',
+                link: '/massoterapia'
+              },
+              {
+                page: 'Rieducazione posturale',
+                link: '/rieducazione-posturale'
+              },
+              {
+                page: 'Pavimento pelvico',
+                link: '/pavimento-pelvico'
+              },
+              {
+                page: 'Tecarterapia',
+                link: '/tecarterapia'
+              },
+              {
+                page: 'Laserterapia',
+                link: '/laserterapia'
+              },
+              {
+                page: 'Onde d urto',
+                link: '/onde-urto'
+              },
+              {
+                page: 'Magnetoterapia',
+                link: '/maggnetoterapia'
+              },
+              {
+                page: 'Tariffario',
+                link: '/tariffario'
+              }
+            ]
+          },
+          { page: 'Osteopatia',
+            link: '/osteopatia'},
+          { page: 'Idrokinesiterapia',
+            link: '/idrokinesiterapia'},
+          { page: 'Riatletizzazione',
+            link: '/riatletizzazione'},
+          { page: 'Neuromotoria',
+            link: 'neuromotoria'},
+          { page: 'Consulenze',
+            link: '/consulenze',
+            submenu: [
+              {
+                page: 'Medici e Centri Collegati',
+                link: '/medici-e-centri-collegati'
+              },
+              {
+                page: 'Nutrizione',
+                link: '/nutrizione'
+              },
+              {
+                page: 'Personal trainer',
+                link: '/personal-trainer'
+              }
+            ]
+          },
+          { page: 'Prodotti',
+            link: 'prodotti'}
+        ]
+      }
+    },
+    methods: {
+      openSubmenu: function(el) {
+        let submenu = el.nextElementSibling;
+        el.classList.toggle('is-active');
+        submenu.classList.toggle('is-expanded');
+      }
+    } 
+  }
 </script>
