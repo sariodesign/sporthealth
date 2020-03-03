0b9dc53240d53c8796befbaf641a2f9f1fbeb248
Rosario <rosario.russo@blastingnews.com>
2020-03-03 17:58:15 +0100

    Started creation of hero component

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,5 @@
-<template>
-    <section class="c-hero">
+<template v-if="">
+    <section class="c-hero" id="hero">
         <div class="c-hero__overlay">
             <p class="text-center">Solo con un approccio multidisciplinare è possibile curare davvero il paziente e farlo tornare a<br>
              condurre una vita piena e soddisfacente.<br>
@@ -7,3 +7,9 @@
         </div>
     </section>
 </template>
+<script>
+    export default {
+
+    }
+    
+</script>
