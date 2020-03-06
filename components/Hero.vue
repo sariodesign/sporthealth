015037ff4256615f3002ed9f9774bbb95c35fee1
Rosario <rosario.russo@blastingnews.com>
2020-03-06 18:20:41 +0100

    Updated Hero component

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="c-hero">
-        <div class="c-hero__content" v-if="typeHero === 'homepage'">
+    <div>
+        <section class="c-hero" v-if="typeHero === 'homepage'">
             <video autoplay class="c-hero__video">
                 <source src="sport-health-intro.webm" type="video/webm">
                 <source src="sport-health-intro.mp4" type="video/mp4">
@@ -9,27 +9,27 @@
                 <figure class="mb-8">
                     <img class="c-hero__logo" src="logo-sporthealth.png" alt="Sport Health Logo" />
                 </figure>
-                <p class="px-4 text-center w-1/2">Solo con un approccio multidisciplinare è possibile curare davvero il paziente e farlo tornare a condurre una vita piena e soddisfacente.<br>
-                <span>(Riccardo Torquati)</span></p>
-                <div class="c-hero_cta">
-                    <button class="o-button o-button--hero">
+                <p class="c-hero__content px-4 text-center w-1/2 mb-4">Solo con un approccio multidisciplinare è possibile curare davvero il paziente e farlo tornare a condurre una vita piena e soddisfacente.<br>
+                <span>Riccardo Torquati</span></p>
+                <div class="c-hero__cta">
+                    <button class="c-hero__cta-item o-button o-button--hero mb-4 xl:mb-0 xl:mr-4">
                         <nuxt-link to="/centri-sporthealth">Scegli il centro più vicino a te</nuxt-link>
                     </button>
-                    <button class="o-button o-button--hero-alt">
+                    <button class="c-hero__cta-item o-button o-button--hero-alt">
                         <nuxt-link to="/centri-sporthealth">Prenota una valutazione specialistica</nuxt-link>
                     </button>
                 </div>
             </div>
-        </div>
+        </section>
 
-        <div class="c-hero__content" v-else-if="typeHero === 'basic'">
+        <section class="c-hero__content" v-else-if="typeHero === 'basic'">
             <h1>Content without video</h1>
-        </div>
+        </section>
 
-        <div class="c-hero__content" v-else>
+        <section class="c-hero__content" v-else>
             <h1>Splitted content</h1>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
 <script>
     export default {
