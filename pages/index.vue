93cc6bbbfc02b2768649cad32f5c3f5a848edf29
Rosario <rosario.russo@blastingnews.com>
2020-04-06 18:52:12 +0200

    Added methods component to index page

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,13 @@
 <script>
 import Hero from '~/components/Hero.vue'
 import MainCategories from '~/components/MainCategories.vue'
+import Methods from '~/components/Methods.vue'
 
 export default {
   components: {
     Hero,
-    MainCategories
+    MainCategories,
+    Methods
   },
   data(){
     return {
